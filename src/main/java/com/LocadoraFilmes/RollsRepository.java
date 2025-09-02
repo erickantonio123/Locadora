022703f7afffb4ba041b7c45fab976eb74efc55f
erickantonio123 <167378644+erickantonio123@users.noreply.github.com>
2025-09-02 16:37:39 -0300

    Delete src directory

--- src/main/java/com/LocadoraFilmes/RollsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.LocadoraFilmes;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RollsRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByNome(String nome);
-}
